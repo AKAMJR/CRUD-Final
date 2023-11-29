8d7a879f55aa407b9173e5d341e7610be02896eb
AKAMJR <diazduranjesus@hotmail.com>
2023-11-29 14:32:03 -0600

    Ahora se pueden agregar alumnos a la tabla

---
+++ agregar.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+    <title>Agregar</title>
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+    <?php
+    if (isset($_POST['enviar'])){
+        $nombre = $_POST['nombre'];
+        $nocontrol = $_POST['Nocontrol'];
+        $escuela = $_POST['escuela'];
+        $semestre = $_POST['semestre'];
+        $sistema = $_POST['sistema'];
+
+        include("conexion.php");
+
+        $sql = "INSERT INTO alumnos (nombre, nocontrol, escuela, semestre, sistema)
+                VALUES ('".$nombre."', '".$nocontrol."', '".$escuela."', '".$semestre."', '".$sistema."')";
+
+        $resultado = mysqli_query($conexion, $sql);
+
+        
+    ?>
+    <h1>Agregar nuevo alumno</h1>
+    <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+        <label>Nombre:</label>
+        <input type="text" name="nombre"><br>
+        <label>No. Control:</label>
+        <input type="text" name="Nocontrol"><br>
+        <label>Escuela:</label>
+        <input type="text" name="escuela"><br>
+        <label>Semestre:</label>
+        <input type="text" name="semestre"><br>
+        <label>Sistema:</label>
+        <input type="text" name="sistema"><br>
+        <input type="submit" name="enviar" value="AGREGAR">
+        <a href="Listas.php">Regresar</a>
+    </form>
+    <?php
+    }
+    ?>
+</body>
+</html>
