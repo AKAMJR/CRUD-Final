9b25ba987776991e02afe33f4783e82074f40302
AKAMJR <diazduranjesus@hotmail.com>
2023-11-29 14:38:43 -0600

    interfaz index o registro

---
+++ index.php
@@ -0,0 +1,28 @@
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <link rel="stylesheet" type="text/css" href="estilo2.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+</head>
+<body>
+
+    <h2>Registro</h2>
+
+    <form method="post" action="">
+        <label for="correo">Correo:</label>
+        <input type="email" id="correo" name="correo" required><br>
+
+        <label for="contrasena">Contraseña:</label>
+        <input type="password" id="contrasena" name="contrasena" required><br>
+
+        <button type="submit">Registrar</button>
+    </form>
+
+    <a href="login.php">¿Ya tienes una cuenta? Inicia sesión aquí</a>
+
+</body>
+</html>
