d3116f3c16f218e1354f9e99bb6405a00e22e012
AKAMJR <diazduranjesus@hotmail.com>
2023-11-29 14:47:32 -0600

    el login lee los datos y compara de la BD que tiene registros para ingresar

--- login.php
+++ login.php
@@ -1,4 +1,42 @@
+<?php
+// Verificar si la sesión no está activa antes de intentar iniciarla
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
+// Verificar si el usuario ya está autenticado, redirigirlo a Listas.php
+if (isset($_SESSION['usuario'])) {
+    header("Location: Listas.php");
+    exit();
+}
+
+// Verificar si se ha enviado el formulario
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Incluir el archivo de conexión
+    include 'conexion2.php';
+
+    // Obtener datos del formulario
+    $correo = $_POST['correo'];
+    $contrasena = $_POST['contrasena'];
+
+    // Consulta SQL para verificar las credenciales
+    $sql = "SELECT * FROM usuarios WHERE correo = '$correo' AND contrasena = '$contrasena'";
+    $result = mysqli_query($conexion, $sql);
+
+    // Verificar si se encontraron resultados
+    if (mysqli_num_rows($result) > 0) {
+        // Iniciar sesión y redirigir a Listas.php
+        $_SESSION['usuario'] = $correo;
+        header("Location: Listas.php");
+        exit();
+    } else {
+        $error_message = "Correo o contraseña incorrectos";
+    }
+
+    // Cerrar la conexión
+    mysqli_close($conexion);
+}
+?>
 
 <!DOCTYPE html>
 <html lang="es">
