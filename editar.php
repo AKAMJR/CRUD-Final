a203743ddf6605bf60060adacf8bbd3592fa8494
AKAMJR <diazduranjesus@hotmail.com>
2023-11-29 14:14:51 -0600

    interfaz conectada a la base de datos

--- editar.php
+++ editar.php
@@ -8,7 +8,49 @@ include("conexion.php");
 
 </head>
 <body>
+    <?php
+    $nombre = $nocontrol = $escuela = $semestre = $sistema = ""; // Inicializa todas las variables
 
+    // Aquí se presiona el botón enviar
+    if (isset($_POST['enviar'])) {
+        $id = $_POST['id'];
+        $nombre = $_POST['nombre'];
+        $nocontrol = $_POST['nocontrol'];
+        $escuela = $_POST['escuela'];
+        $semestre = $_POST['semestre'];
+        $sistema = $_POST['sistema'];
+
+        $sql = "UPDATE alumnos SET nombre='".$nombre."', nocontrol='".$nocontrol."', escuela='".$escuela."', semestre='".$semestre."', sistema='".$sistema."' WHERE id='".$id."'";
+        $resultado = mysqli_query($conexion, $sql);
+
+        if ($resultado) {
+            echo "<script language='JavaScript'>
+            alert('Los datos se actualizaron correctamente');
+            location.assign('Listas.php');
+            </script>";
+        } else {
+            echo "<script language='JavaScript'>
+            alert('Los datos NO se actualizaron correctamente');
+            location.assign('Listas.php');
+            </script>";
+        }
+        mysqli_close($conexion);
+
+    } else {
+        // Aquí entra si no se ha presionado el botón
+
+        $id = $_GET['id'];
+        $sql = "SELECT * FROM alumnos WHERE id='".$id."'";
+        $resultado = mysqli_query($conexion, $sql);
+
+        $fila = mysqli_fetch_assoc($resultado);
+        $nombre = $fila["nombre"];
+        $nocontrol = $fila["nocontrol"];
+        $escuela = $fila["escuela"];
+        $semestre = $fila["semestre"];
+        $sistema = $fila["sistema"];
+        mysqli_close($conexion);
+    ?>
     <h1>Editar alumno</h1>
     <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
         <label>Nombre:</label>
@@ -32,7 +74,7 @@ include("conexion.php");
         <a href="Listas.php">Regresar</a>
     </form>
     <?php
-    
+    }
     ?>
 </body>
 </html>
