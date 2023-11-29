70ed1c576fb832c30b0a23b7656867621eb22df3
AKAMJR <diazduranjesus@hotmail.com>
2023-11-29 13:28:53 -0600

    conexion fallida

---
+++ conexion.php
@@ -0,0 +1,12 @@
+<?php
+
+$dbname = "id21577392_escuela";
+$dbuser = "id21577392_root";
+$dbhost = "localhost";
+$dbpass = "Jesus9910.";
+
+$conexion = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+
+
+?>
